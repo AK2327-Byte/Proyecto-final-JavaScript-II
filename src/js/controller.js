003bf0ce7599852e30a0e98bcd0bd1e9d1900bcf
AK2327-Byte <knightkan23@gmail.com>
2026-09-09 19:10:57 -0600

    Clean: repositorio limpio y optimizado para Netlify

---
+++ src/js/controller.js
@@ -0,0 +1,67 @@
+import * as model from './model.js';
+import recipeView from './views/RecipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/ResultsView.js';
+import paginationView from './views/paginationView.js';
+
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 0. Actualizar resultados AL INSTANTE para marcar la receta activa
+    if (model.state.search.results.length > 0) {
+      // Usamos 'render' porque 'update' aún no está implementado en View.js
+      resultsView.render(model.getSearchResultsPage()); 
+    }
+
+    // 1. Cargar receta
+    await model.loadRecipe(id);
+
+    // 2. Renderizar receta
+    recipeView.render(model.state.recipe);
+    
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1. Obtener la consulta de búsqueda
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Cargar resultados
+    await model.loadSearchResults(query);
+
+    // 3. Renderizar resultados en la lista lateral
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4. Renderizar la paginación inicial
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1. Renderizar NUEVOS resultados
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2. Renderizar NUEVOS botones de paginación
+  paginationView.render(model.state.search);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
